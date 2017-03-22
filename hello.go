9cd4a7e340b85abf2d262abccb3d9ea0f6ab3443
Saifudeen Thazhe Kunnummal <saifudeentk@yahoo.com>
2017-03-21 18:36:21 -0700

    THIRD commit

--- hello.go
+++ hello.go
@@ -1,11 +1,12 @@
 package main
 
-
-import  "fmt"
+import "fmt"
 
 func main() {
-
-fmt.Println("Hello Bhai")
+	x := 2
+	y := 3
+	z := 0
+	fmt.Println("Hello Boss")
+	z = x + y
+	fmt.Println("Value is ", z)
 }
-
-
--- hello1.go
+++ hello1.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main(){
+func main() {
+	x := 2
+	y := 3
+	z := 0
 	fmt.Println("Hello Boss")
+	z = x + y
+	fmt.Println("Value is ", z)
 }
